86a9a5dc297d3c4519f59de005e0a100555326ff
Rubens Cordeiro <zyoncore@hotmail.com>
2023-05-12 08:53:08 -0300

    versao 23.5.11.193

--- src/Snebur.VisualStudio2022/source.extension.cs
+++ src/Snebur.VisualStudio2022/source.extension.cs
@@ -11,7 +11,7 @@ namespace Snebur.VisualStudio
         public const string Name = "Snebur.VisualStudio2022";
         public const string Description = @"Snebur extensão ";
         public const string Language = "en-US";
-        public const string Version = "23.4.25.190";
+        public const string Version = "23.5.11.193";
         public const string Author = "Rubens Cordeiro";
         public const string Tags = "";
     }
