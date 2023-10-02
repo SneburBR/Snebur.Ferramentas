6b37592c23f3ed1b93d19fefa15b5ff415df35b9
Rubens Cordeiro <zyoncore@hotmail.com>
2023-10-02 07:57:57 -0300

    adicionado ItensControleUtil

---
+++ src/Snebur.VisualStudio2022/Utilidade/ItensControlUtil.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Snebur.VisualStudio
+{
+    public static class ItensControlUtil
+    {
+        public static async Task ScrollToBottonAsync(ItemsControl itemsControl)
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+            if (VisualTreeHelper.GetChildrenCount(itemsControl) > 0)
+            {
+                try
+                {
+                    Border border = (Border)VisualTreeHelper.GetChild(itemsControl, 0);
+                    ScrollViewer scrollViewer = (ScrollViewer)VisualTreeHelper.GetChild(border, 0);
+                    scrollViewer.ScrollToBottom();
+                }
+                catch
+                {
+
+                }
+            }
+        }
+    }
+}
