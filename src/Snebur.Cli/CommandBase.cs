0a04647924556364796e54a0646e3f0302aed61d
Rubens Cordeiro <zyoncore@hotmail.com>
2024-06-16 11:49:48 +0200

    added CommandBase

---
+++ src/Snebur.Cli/CommandBase.cs
@@ -0,0 +1,11 @@
+﻿using System.CommandLine;
+
+namespace Snebur.Cli;
+
+abstract class CommandBase : Command
+{
+    protected CommandBase(string name, string? description = null) : base(name, description)
+    {
+    }
+}
+
