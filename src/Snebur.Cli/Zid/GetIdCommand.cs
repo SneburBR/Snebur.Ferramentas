e654352ed34fbc2cca1f125aa800b7fbe841638d
Rubens Cordeiro <zyoncore@hotmail.com>
2024-06-16 11:49:57 +0200

    Added GetIdCommand

---
+++ src/Snebur.Cli/Zid/GetIdCommand.cs
@@ -0,0 +1,25 @@
+﻿using Snebur.Utilidade;
+using System.CommandLine;
+using System.CommandLine.NamingConventionBinder;
+
+namespace Snebur.Cli.Zid;
+
+class GetIdCommand : CommandBase
+{
+    //write a descript for this command, that get Id generated hash called Zid (Zyoncore Id) Ex: 1 1 => 15656
+
+    public GetIdCommand()
+        : base("--get-id", "Get Id from Zyoncore Id (Zid)")
+    {
+        AddArgument(new Argument<int>("zid", "Id to generate Zid"));
+
+        this.Handler = CommandHandler.Create<int>(HandleCommand);
+
+    }
+
+    private void HandleCommand(int zid)
+    {
+        Console.WriteLine($"Id: {zid} => {ZidUtil.RetornarId(zid)}");
+    }
+}
+
