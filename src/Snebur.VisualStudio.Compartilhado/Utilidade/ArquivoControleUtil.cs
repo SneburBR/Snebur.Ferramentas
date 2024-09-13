ba81ea9afebcb7580efde5f655d99534a2099869
Rubens Cordeiro <zyoncore@hotmail.com>
2024-09-13 10:07:31 +0200

    Add TypescriptDefinicaoUtil for parsing TypeScript definitions
    Introduce TypescriptDefinicaoUtil in Snebur.VisualStudio namespace.
    Add methods RetornarCaminhosClasses and ExtractNamespacesAndClasses.
    Use regex to extract namespaces and classes from TypeScript content.
    Map namespaces to classes and build fully qualified class paths.
    Utilize System.Text.RegularExpressions.Regex for pattern matching.
    Leverage ExpressaoUtil.RetornarExpressaoAbreFecha for namespace content.

--- src/Snebur.VisualStudio.Compartilhado/Utilidade/ArquivoControleUtil.cs
+++ src/Snebur.VisualStudio.Compartilhado/Utilidade/ArquivoControleUtil.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Snebur.VisualStudio
 {
@@ -65,5 +66,58 @@ namespace Snebur.VisualStudio
         }
     }
 
+    public static class TypescriptDefinicaoUtil
+    {
+        internal static IEnumerable<string> RetornarCaminhosClasses(string caminhoDefinicacao)
+        {
+            var content = File.ReadAllText(caminhoDefinicacao);
+            return ExtractNamespacesAndClasses(content);
+        }
 
+        static IEnumerable<string> ExtractNamespacesAndClasses(string content)
+        {
+            var namespacePattern = @"declare\s+namespace\s+([\w\.]+)\s*\{";
+            var classPattern = @"\b(?:class|abstract\s+class|enum)\s+([\w]+)\s";
+
+            var namespaceMatches = Regex.Matches(content, namespacePattern);
+            var classesPerNamespace = new Dictionary<string, List<string>>();
+
+            foreach (Match namespaceMatch in namespaceMatches)
+            {
+                var namespaceName = namespaceMatch.Groups[1].Value;
+                var namespaceStartIndex = namespaceMatch.Index + namespaceMatch.Length;
+                var subContext = content.Substring(namespaceMatch.Index);
+                var namespaceContent = ExpressaoUtil.RetornarExpressaoAbreFecha(subContext, false, '{', '}');
+
+                var namespaceEndIndex = content.IndexOf("}", namespaceStartIndex);
+ 
+                //string namespaceContent = content.Substring(namespaceStartIndex, namespaceEndIndex - namespaceStartIndex);
+                var classMatches = Regex.Matches(namespaceContent, classPattern);
+
+                foreach (Match classMatch in classMatches)
+                {
+                    string className = classMatch.Groups[1].Value;
+
+                    if (!classesPerNamespace.ContainsKey(namespaceName))
+                    {
+                        classesPerNamespace[namespaceName] = new List<string>();
+                    }
+
+                    classesPerNamespace[namespaceName].Add(className);
+                }
+            }
+
+            var caminhosClasses = new HashSet<string>();
+            foreach (var dominio in classesPerNamespace.Keys)
+            {
+                var classes = classesPerNamespace[dominio];
+                foreach(var classe in classes)
+                {
+                    var caminho = $"{dominio}.{classe}";
+                    caminhosClasses.Add(caminho);
+                }
+            }
+            return caminhosClasses;
+        }
+    }
 }
